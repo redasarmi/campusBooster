ee99ec6115e0cdf58a616434e1b8b378bda37748
reda.dev.s <sarmi.reda.dev@gmail.com>
2022-06-01 14:40:17 +0200

    Initial Commit

---
+++ campusBooster/campusBoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  campusBoosterApp.swift
+//  campusBooster
+//
+//  Created by Reda Sarmi on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct campusBoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
